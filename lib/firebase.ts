bea6697dade32aebc9dbf790ab9dbc7f8f5427b1
Satish Banchhere <satishbanchhere1@gmail.com>
2025-10-14 20:13:13 +0530

    firebase shifted

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,15 +4,14 @@ import { getAuth } from "firebase/auth";
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhYqkRn5rT4nhky9tQFXA2d_NNagvnw18",
-  authDomain: "kk-mishra-test.firebaseapp.com",
-  projectId: "kk-mishra-test",
-  storageBucket: "kk-mishra-test.firebasestorage.app",
-  messagingSenderId: "846351551174",
-  appId: "1:846351551174:web:dd8c2b2949fc7b1c20e5c7",
-  measurementId: "G-VS9S3V0M0R"
+  apiKey: "AIzaSyC7j43oGfB8p4Gbqoc5H0fcVR4K_Sfy7FM",
+  authDomain: "abc-classes-18b7a.firebaseapp.com",
+  projectId: "abc-classes-18b7a",
+  storageBucket: "abc-classes-18b7a.firebasestorage.app",
+  messagingSenderId: "851915301949",
+  appId: "1:851915301949:web:f47e201ecb70a54f643654",
+  measurementId: "G-KJH30Q1254"
 };
-
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
